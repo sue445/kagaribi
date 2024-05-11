96b7d6da1377ac27f0a3b7f866ebb59184cacab5
sue445 <sue445@sue445.net>
2024-05-11 18:33:20 +0900

    Impl `Collection#get`

--- lib/kagaribi/collection.rb
+++ lib/kagaribi/collection.rb
@@ -2,6 +2,8 @@
 
 module Kagaribi
   class Collection
+    MAX_RETRY_COUNT = 5
+
     # @!attribute [r] collection_name
     #   @return [String]
     attr_reader :collection_name
@@ -28,6 +30,16 @@ module Kagaribi
     def set(doc_key, data)
       ref = firestore.doc(full_doc_key(doc_key))
       ref.set(data)
+    end
+
+    # @param doc_key [String]
+    # @return [Hash] return empty Hash if document isn't found
+    def get(doc_key)
+      with_retry("Kagaribi::Collection#get") do
+        ref = firestore.doc(full_doc_key(doc_key))
+        snap = ref.get
+        snap&.data || {}
+      end
     end
 
     # @param key [String]
@@ -58,6 +70,46 @@ module Kagaribi
     # @return [String]
     def full_doc_key(doc_key)
       "#{sanitized_collection_name}/#{sanitize_key(doc_key)}"
+    end
+
+    # @param label [String]
+    # @yield
+    def with_retry(label)
+      yield
+    rescue TypeError, GRPC::Unavailable, RuntimeError, Signet::AuthorizationError => e
+      raise e unless retryable_error?(e)
+
+      retry_count ||= 0
+      retry_count += 1
+
+      raise e if retry_count > MAX_RETRY_COUNT
+
+      logger.warn "[#{label}] collection_name=#{@collection_name}, retry_count=#{retry_count}, error=#{e}"
+      sleep 1
+      retry
+    end
+
+    # @param error [StandardError]
+    # @return [Boolean]
+    def retryable_error?(error)
+      case error
+      when RuntimeError
+        # e.g.
+        # Could not load the default credentials. Browse to
+        # https://developers.google.com/accounts/docs/application-default-credentials
+        # for more information
+        return true if error.message.include?("Could not load the default credentials.")
+
+        # e.g.
+        # Your credentials were not found. To set up Application Default
+        # Credentials for your environment, see
+        # https://cloud.google.com/docs/authentication/external/set-up-adc
+        return true if error.message.include?("Your credentials were not found.")
+
+        return false
+      end
+
+      true
     end
   end
 end
